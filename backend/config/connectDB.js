6f325da0465e111b58772fa69d822739debb9ec1
Esar Fatima <esaarfatima16@yahoo.com>
2025-04-20 11:40:56 +0500

    changes in connectDB

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -1,19 +1,3 @@
-/* module.exports = {
-  DB_HOST: process.env.DB_HOST || 'DESKTOP-RPDH2ER',  // Or 'localhost' if on same machine
-  DB_NAME: process.env.DB_NAME || 'Project1',
-  DB_PORT: process.env.DB_PORT || 1433,               // Default SQL Server port
-  NODE_ENV: process.env.NODE_ENV || 'development',
-  
-  // Windows Authentication specific
-  DB_TRUSTED_CONNECTION: process.env.DB_TRUSTED_CONNECTION || true,
-  
-  // Optional for debugging
-  DB_LOG: process.env.DB_LOG || false
-};
-
-const connectionString = `Driver={SQL Server Native Client 11.0};Server=${config.DB_HOST};Database=${config.DB_NAME};Trusted_Connection=${config.DB_TRUSTED_CONNECTION ? 'Yes' : 'No'};`;
-
-module.exports = connectionString; */
 
 const config = {
   DB_HOST: process.env.DB_HOST || 'DESKTOP-RPDH2ER',  // Or 'localhost' if on the same machine
